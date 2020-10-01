47f4374694703ef24e31e49c656b11d366885858
maloleg <33466049+maloleg@users.noreply.github.com>
2020-10-02 00:51:47 +0300

    final one

--- console.cpp
+++ console.cpp
@@ -12,7 +12,6 @@ void Console::run()
 
     for (int64_t i = 0; i < files.size(); i++){
         v.push_back(std::make_pair(files[i].fileName().toUtf8().constData(), files[i].size()));
-        // std::cout << files[i].fileName().toUtf8().constData() << "   " << files[i].size() << "\n";
         AvgSize += files[i].size();
     }
   
@@ -28,7 +27,6 @@ void Console::run()
     connect(this, SIGNAL(Exit()), this, SLOT(SLOT_Exit())); 
     connect(this, SIGNAL(TimerChange(uint64_t)), this, SLOT(SLOT_Timeout(uint64_t)));
     connect(tmr, SIGNAL(timeout()), this, SLOT(SLOT_WhatToDo()));
-    tmr->start();
 
 }
 
@@ -42,14 +40,15 @@ void Console::readCommand()
     else if (regex_match(s.begin(), s.end(), std::regex("exit"))) emit Exit();
     else if (regex_match(s.begin(), s.end(), std::regex("restart"))) emit Restart();
     else if (regex_match(s.begin(), s.end(), std::regex("timeout\\s\\d+"))) {emit TimerChange(std::stoi(s.substr(8, s.size() - 8).c_str())); std::cout << ">" << std::flush;}
+    else std::cout << ">" << std::flush;
 
 }
 
 void Console::SLOT_Start(){
-        Is_started = 1;
+    tmr->start();
 }
 void Console::SLOT_Stop(){
-    Is_started = 0;
+    tmr->stop();
 }
 void Console::SLOT_Exit(){
     QCoreApplication::exit(1337);
@@ -60,35 +59,30 @@ void Console::SLOT_Timeout(uint64_t t){
 }
 
 void Console::SLOT_WhatToDo(){
-
-    if (Is_started) {
-        AvgSize = 0;
-        
-        if (v.size() != 0){
-            std::cout << "\n{";
-            for (uint64_t i = 0; i < v.size(); i++){
-                if (i != v.size() - 1) std::cout << v[i].first << ", ";
-                else std::cout << v[i].first;
-                AvgSize += v[i].second;
-            }
-
-            if (v.size() != 0)std::cout << "}\n>" << std::flush;
-            // std::cout << ">" << std::flush();
-            AvgSize /= v.size();
-            AvgSize *= 0.75;
-            min = abs(v[0].second-AvgSize);
-            min_n = 0;
-
-            for (uint64_t i = 0; i < v.size(); i++){
-                if (abs(AvgSize - v[i].second) < min){
-                    min = abs(AvgSize - v[i].second);
-                    min_n = i;
-                }
-            }
-            // std::cout << "AvgSize = " << AvgSize << " min = " << min << " min_n = " << min_n << "\n>" << std::flush;
-            
-            v.erase(v.begin() + min_n);
+    AvgSize = 0;
+    
+    if (v.size() != 0){
+        std::cout << "\n{";
+        for (uint64_t i = 0; i < v.size(); i++){
+            if (i != v.size() - 1) std::cout << v[i].first << ", ";
+            else std::cout << v[i].first;
+            AvgSize += v[i].second;
         }
-        else {std::cout << "No more elements, stopped\n>" << std::flush; emit Stop();} 
+
+        if (v.size() != 0)std::cout << "}\n>" << std::flush;
+        AvgSize /= v.size();
+        AvgSize *= 0.75;
+        min = abs(v[0].second-AvgSize);
+        min_n = 0;
+
+        for (uint64_t i = 0; i < v.size(); i++){
+            if (abs(AvgSize - v[i].second) < min){
+                min = abs(AvgSize - v[i].second);
+                min_n = i;
+            }
+        }
+        
+        v.erase(v.begin() + min_n);
     }
+    else {std::cout << "No more elements, stopped\n>" << std::flush; emit Stop();} 
 }
